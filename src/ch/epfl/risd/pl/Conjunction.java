e36a0dee52df2a4d306e24ca65fc161b48726789
IlievskiV <ilievski.vladimir@live.com>
2016-06-09 14:21:56 +0200

    Adding class Conjunction

---
+++ src/ch/epfl/risd/pl/Conjunction.java
@@ -0,0 +1,71 @@
+package ch.epfl.risd.pl;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This class represents a Conjunction.
+ */
+public class Conjunction extends Formula {
+
+	/****************************************************************************/
+	/* VARIABLES */
+	/***************************************************************************/
+
+	/* The list of sub-formulas of which the conjunction is consisted */
+	private List<Formula> subFormulas;
+
+	/****************************************************************************/
+	/* PUBLIC METHODS */
+	/***************************************************************************/
+
+	/* Constructor for empty list of sub-formulas */
+	public Conjunction() {
+		subFormulas = new LinkedList<Formula>();
+	}
+
+	/* Constructor for conjunction of two formulas */
+	public Conjunction(Formula formula1, Formula formula2) {
+		subFormulas = new LinkedList<Formula>();
+		subFormulas.add(formula1);
+		subFormulas.add(formula2);
+	}
+
+	/* Constructor for the conjunction of list of sub-formulas */
+	public Conjunction(List<Formula> subFormulas) {
+		this.subFormulas = subFormulas;
+	}
+	
+	
+	@Override
+	public Formula calculate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String evaluate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		/* The resulting string */
+		StringBuilder sb = new StringBuilder();
+
+		/* Iterate over the sub-formulas */
+		for (Formula f : subFormulas) {
+			sb.append(f.toString() + "*");
+		}
+
+		/* Cut the last * */
+		sb.setLength(sb.length() - 1);
+		return sb.toString();
+	}
+	
+	/* Get the list of sub-formulas */
+	public List<Formula> getSubFormulas() {
+		return subFormulas;
+	}
+}
