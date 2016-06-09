07b29c515ab94acc158c0dc50d1e5e3e002f2850
ilievski <vladimir.ilievski@epfl.ch>
2016-06-09 23:58:14 +0200

    Adding skeletons for testing classes

---
+++ test/ch/epfl/risd/pl/test/EvaluationTests.java
@@ -0,0 +1,12 @@
+package ch.epfl.risd.pl.test;
+
+import org.junit.Test;
+
+public class EvaluationTests {
+	
+	@Test
+	public void test1(){
+		
+	}
+	
+}
---
+++ test/ch/epfl/risd/pl/test/PrintingTests.java
@@ -0,0 +1,12 @@
+package ch.epfl.risd.pl.test;
+
+import org.junit.Test;
+
+public class PrintingTests {
+
+	@Test
+	public void test1() {
+
+	}
+
+}
