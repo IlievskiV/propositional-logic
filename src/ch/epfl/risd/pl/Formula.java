47d8513ba2c52cdee6223c6f5487e80966f48f11
IlievskiV <ilievski.vladimir@live.com>
2016-06-09 12:57:38 +0200

    Adding class Formula

---
+++ src/ch/epfl/risd/pl/Formula.java
@@ -0,0 +1,10 @@
+package ch.epfl.risd.pl;
+
+/**
+ * Base class for the propositional logic.
+ */
+public abstract class Formula {
+
+	/* Function to evaluate the formula */
+	public abstract Formula evaluate();
+}
